618ed7063442bf10fcf55306fcb3695580577496
William Porter Westling <porter@datavant.com>
2022-12-20 17:49:34 -0800

    20-2

--- src/BTreeList.hs
+++ src/BTreeList.hs
@@ -9,6 +9,7 @@ import Debug.Trace
 import Safe (headMay)
 import Data.Maybe (isNothing)
 
+
 data BTreeList a = BTreeBranch { branchSize :: Int, branches :: [BTreeList a] } | BTreeLeaf{leafSize :: Int, values :: [a] } deriving (Show, Eq, Ord)
 
 size :: BTreeList a -> Int
@@ -25,8 +26,8 @@ get index (BTreeBranch _ xs) = result where
 targetSize :: Int
 targetSize = 64
 
-maybeSplit :: BTreeList a -> BTreeList a
-maybeSplit b@(BTreeLeaf n xs) = if n > (targetSize * targetSize) then result else b where
+maybeSplit :: (Show a) => BTreeList a -> BTreeList a
+maybeSplit b@(BTreeLeaf n xs) = if n > (targetSize * targetSize) then (result) else b where
   groups = chunksOf targetSize xs
   makeLeaf xs = BTreeLeaf (length xs) xs
   result = BTreeBranch n (map makeLeaf groups)
@@ -38,18 +39,18 @@ toList (BTreeBranch _ xs) = concatMap toList xs
 insert :: (Show a) => Int -> a -> BTreeList a -> BTreeList a
 insert index value (BTreeLeaf s xs) = maybeSplit $ BTreeLeaf (s + 1) (take index xs ++ [value] ++ drop index xs)
 insert index value (BTreeBranch s xs) = BTreeBranch (s + 1) (result) where
-  next :: (Show a) => Int -> Int -> [BTreeList a] -> (Int, Int, BTreeList a)
-  next i n (branch: branches) = if ((n > size branch)) then next (i+1) (( n) - (size ( branch))) branches else (i, n, branch)
-  next i n [] = ((error "index out of bounds"))
+  next :: Int -> Int -> [BTreeList a] -> (Int, Int, BTreeList a)
+  next i n (branch: branches) = if ((n >= size branch)) then next (i+1) (( n) - (size ( branch))) branches else ((i, n, branch))
+  next i n [] = (i, n, BTreeLeaf 0 [])
   (i, n, branch) = next 0 index xs
   newBranch = insert ( n) value ( branch)
   result = take i xs ++ [newBranch] ++ drop (i+1) xs
 
 remove :: (Show a) => Int -> BTreeList a -> BTreeList a
-remove index (BTreeLeaf s xs) = BTreeLeaf (s - 1) (take index xs ++ drop (index + 1) xs)
+remove index l@(BTreeLeaf s xs) = BTreeLeaf (s - 1) (take index xs ++ drop (index + 1) xs)
 remove index (BTreeBranch s xs) = BTreeBranch (s - 1) (result) where
-  next :: (Show a) => Int -> Int -> [BTreeList a] -> (Int, Int, BTreeList a)
-  next i n (branch: branches) = if ((n > size branch)) then next (i+1) (( n) - (size ( branch))) branches else (i, n, branch)
+  next :: Int -> Int -> [BTreeList a] -> (Int, Int, BTreeList a)
+  next i n (branch: branches) = if ((n >= size branch)) then next (i+1) (( n) - (size ( branch))) branches else (i, n, branch)
   next i n [] = ((error "index out of bounds"))
   (i, n, branch) = next 0 index xs
   newBranch = remove ( n) ( branch)
@@ -64,6 +65,13 @@ findIndex f (BTreeBranch _ xs) = result where
   indexOfMatch = length noMatchBranches
   result = if indexOfMatch >= length xs then Nothing else fmap (\(value, i) -> (value, i + sum (map size (take indexOfMatch xs)))) (searches !! indexOfMatch)
 
+
+empty :: BTreeList a
+empty = BTreeLeaf 0 []
+
+toBTreeList :: (Show a) => [a] -> BTreeList a
+toBTreeList xs = result where
+  result = foldl (\btreeList (i, x) -> insert i x btreeList) empty (zip [0..] xs)
 
 
 
